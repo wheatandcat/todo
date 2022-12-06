7cc5e8d8e2168cadb86d0dc8a416aa57e2c7bcac
wheatandcat <wheatandcat@gmail.com>
2022-12-07 00:44:28 +0900

    update

--- src/components/organisms/Preview.tsx
+++ src/components/organisms/Preview.tsx
@@ -15,55 +15,80 @@ const Preview: React.FC<Props> = ({ markdown, onChangeTask }) => {
           overrides: {
             li: {
               component: (props) => {
-                if (!props.children) return null;
-                const c0 = props.children[0];
+                try {
+                  if (!props.children) return null;
+                  const c0 = props.children[0];
 
-                if (typeof c0 === "string") {
-                  return <li>{props.children}</li>;
-                }
+                  if (typeof c0 === "string") {
+                    return <li>{props.children}</li>;
+                  }
 
-                const checked = c0.props.checked;
-                const taskText = props.children[1]
-                  .replaceAll("\n", "")
-                  .replaceAll(" ", "");
+                  const checked = c0.props.checked;
 
-                const id: string = `checkbox-${taskText}`;
+                  const last = props.children.at(-1);
+                  let nest: string[] = [];
 
-                const last = props.children.at(-1);
-                let nest: string[] = [];
+                  if (typeof last !== "string" && last.type === "ul") {
+                    nest = last.props.children.map(
+                      (v: any) => v.props.children[0]
+                    );
+                  }
 
-                if (typeof last !== "string") {
-                  nest = last.props.children.map(
-                    (v: any) => v.props.children[0]
+                  let taskText = props.children[1]
+                    .replaceAll("\n", "")
+                    .replaceAll(" ", "");
+
+                  if (last.type === "a") {
+                    taskText = last.props.children[0]
+                      .replaceAll("\n", "")
+                      .replaceAll(" ", "");
+                  }
+
+                  const id: string = `checkbox-${taskText}`;
+
+                  const isText = last.type !== "a";
+
+                  return (
+                    <li {...props}>
+                      <div className="flex items-center">
+                        <div className="pr-2 pt-1">
+                          <input
+                            id={id}
+                            type="checkbox"
+                            defaultChecked={checked}
+                            aria-labelledby="task item"
+                            readOnly={false}
+                            onChange={() => {
+                              onChangeTask(checked, taskText, nest);
+                            }}
+                          />
+                        </div>
+                        <div>
+                          <label htmlFor={id}>
+                            <span>{isText ? taskText : last}</span>
+                          </label>
+                        </div>
+                      </div>
+                      {nest.map((v: string, index: number) => (
+                        <List key={index} text={v} />
+                      ))}
+                    </li>
+                  );
+                } catch (e) {
+                  console.log("props", props);
+                  console.log("エラー内容", e);
+
+                  return (
+                    <div>
+                      <span className="text-red-600 font-bold text-sm underline underline-offset-2">
+                        ・☠️ パースに失敗しました{" "}
+                      </span>
+                      <span className="text-white text-sm font-normal">
+                        （元: {props.children}）
+                      </span>
+                    </div>
                   );
                 }
-
-                return (
-                  <li {...props}>
-                    <div className="flex items-center">
-                      <div className="pr-2 pt-1">
-                        <input
-                          id={id}
-                          type="checkbox"
-                          defaultChecked={checked}
-                          aria-labelledby="task item"
-                          readOnly={false}
-                          onChange={() => {
-                            onChangeTask(checked, taskText, nest);
-                          }}
-                        />
-                      </div>
-                      <div>
-                        <label htmlFor={id}>
-                          <span>{taskText}</span>
-                        </label>
-                      </div>
-                    </div>
-                    {nest.map((v: string, index: number) => (
-                      <List key={index} text={v} />
-                    ))}
-                  </li>
-                );
               },
             },
           },
