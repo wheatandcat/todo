264fa6248e317f031b1cd65a203349b137fe0156
Yohei Iino <wheatandcat@gmail.com>
2023-01-22 20:28:35 +0900

    Vitest導入 (#26)
    * add: vitest
    
    * add: vitest
    
    * add: vitest
    
    * fix

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: ["./setupTests.ts"],
+    css: true,
+  },
+});
